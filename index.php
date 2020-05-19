299f28974f1d70a60a004e4777b23946897c58cc
Gregory <goutgregoryy@gmail.com>
2020-05-19 17:00:18 +0200

    plateforme

--- index.php
+++ index.php
@@ -136,6 +136,7 @@ function insertComment() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css\app.css">
     <title>La Plateforme</title>
 </head>
 <body>
