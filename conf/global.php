53dfaed4ae0d025a840bcb3864616c1d48d0027d
whiteangel13750 <61412972+whiteangel13750@users.noreply.github.com>
2020-08-31 20:45:22 +0200

    Delete global.php

--- conf/global.php
+++
@@ -1,9 +0,0 @@
-<!-- Le Global qui contient les identifiants de la base de donnée -->
-
-<?php
-
-define('DATABASE','mysql:host=localhost;dbname=plateforme;charset=utf8');
-define('LOGIN', 'root');
-define('PASSWD', '');
-
-?>
